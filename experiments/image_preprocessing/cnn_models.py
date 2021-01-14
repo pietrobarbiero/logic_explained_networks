f9be97a4766e6febd7aed9c58ad6909e406fdf7c
Gaber <gabriele.ciravegna@unifi.it>
2021-01-14 17:21:42 +0100

    Added Inception Model to cnn_models and pretraining

--- experiments/image_preprocessing/cnn_models.py
+++ experiments/image_preprocessing/cnn_models.py
@@ -1,46 +1,39 @@
 import torch
+import torchvision
 from torchvision.models import ResNet
-from torchvision.models.resnet import BasicBlock, Bottleneck
+from torchvision.models.resnet import BasicBlock
 
 RESNET10 = "Resnet_10"
 RESNET18 = "Resnet_18"
 RESNET34 = "Resnet_34"
 RESNET50 = "Resnet_50"
 RESNET101 = "Resnet_101"
+INCEPTION = "Inception"
+
+inception = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=True)
 
 
-def ResNet10():
+def ResNet10(pretrained=False):
+    assert pretrained == False, "No pretrained weights available for ResNet10"
     return ResNet(BasicBlock, [1, 1, 1, 1])
-
-
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-
-
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
-
-
-def ResNet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
-
-
-def ResNet101():
-    return ResNet(Bottleneck, [3, 4, 23, 3])
 
 
 CNN_MODELS = {
     RESNET10: ResNet10,
-    RESNET18: ResNet18,
-    RESNET34: ResNet34,
-    RESNET50: ResNet50,
-    RESNET101: RESNET101,
+    RESNET18: torchvision.models.resnet18,
+    RESNET34: torchvision.models.resnet34,
+    RESNET50: torchvision.models.resnet50,
+    RESNET101: torchvision.models.resnet101,
+    INCEPTION: torchvision.models.inception_v3,
 }
 
 
-def get_model(model_name, n_classes):
+def get_model(model_name, n_classes, pretrained=True):
     model_dict = CNN_MODELS[model_name]
-    resnet = model_dict()
-    feat_dim = resnet.fc.weight.shape[1]
-    resnet.fc = torch.nn.Linear(feat_dim, n_classes)
-    return resnet
+    model = model_dict(pretrained=pretrained)
+    feat_dim = model.fc.weight.shape[1]
+    model.fc = torch.nn.Linear(feat_dim, n_classes)
+    if model_name == INCEPTION:
+        aux_feat_dim = model.AuxLogits.fc.in_features
+        model.AuxLogits.fc = torch.nn.Linear(aux_feat_dim, n_classes)
+    return model
