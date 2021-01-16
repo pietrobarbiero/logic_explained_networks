b0052dc5c463b51410cab83cb7107353586b20ae
Pietro <pietro.barbiero9@live.it>
2021-01-16 12:33:48 +0000

    Add feature names as parameter to get explanations with original names

--- deep_logic/fol/relunn.py
+++ deep_logic/fol/relunn.py
@@ -1,4 +1,5 @@
 from typing import Tuple, List
+import collections
 
 import torch
 import numpy as np
@@ -50,7 +51,7 @@ def generate_local_explanations(model: torch.nn.Module, x_sample: torch.Tensor, 
             if concept_names:
                 term = concept_names[j]
             else:
-                term = f'f{j}'
+                term = f'feature{j:05}'
 
             if xij >= 0.5:
                 explanation += term
@@ -61,20 +62,22 @@ def generate_local_explanations(model: torch.nn.Module, x_sample: torch.Tensor, 
 
 
 def combine_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch.Tensor,
-                               k: int = 5, concept_names: List = None,
-                               device: torch.device = torch.device('cpu')) -> Tuple[str, np.array]:
+                               max_concepts: int = 5, topk_explanations: int = 2, concept_names: List = None,
+                               device: torch.device = torch.device('cpu')) -> Tuple[str, np.array, collections.Counter]:
     """
     Generate a global explanation combining local explanations.
 
     :param model: pytorch model
     :param x: input samples
     :param y: target labels
-    :param k: upper bound to the number of symbols involved in the explanation (it controls the complexity of the explanation)
+    :param max_concepts: upper bound to the number of concepts involved in a local explanation (it controls the complexity of the local explanation)
+    :param topk_explanations: number of most common local explanations to combine in a global explanation (it controls the complexity of the global explanation)
     :param concept_names: list containing the names of the input concepts
     :param device: cpu or cuda device
-    :return: global explanation and predictions
+    :return: Global explanation, predictions, and ranking of local explanations
     """
     local_explanations = []
+    local_explanations_translated = []
     # TODO: multi class
     for xi, yi in zip(x, y):
         # get reduced model for each sample
@@ -83,11 +86,22 @@ def combine_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch
 
         # generate local explanation only if the prediction is correct
         if output > 0.5 and (output > 0.5) == yi:
-            local_explanation = generate_local_explanations(model_reduced, xi, k, concept_names=None, device=device)
+            local_explanation = generate_local_explanations(model_reduced, xi, max_concepts, concept_names=None, device=device)
             local_explanations.append(local_explanation)
+            local_explanation_translated = generate_local_explanations(model_reduced, xi, max_concepts, concept_names=concept_names, device=device)
+            local_explanations_translated.append(local_explanation_translated)
+
+    counter = collections.Counter(local_explanations)
+    if len(counter) < topk_explanations:
+        topk_explanations = len(counter)
+    most_common_explanations = []
+    for explanation, _ in counter.most_common(topk_explanations):
+        most_common_explanations.append(explanation)
+
+    counter_translated = collections.Counter(local_explanations_translated)
 
     # the global explanation is the disjunction of local explanations
-    global_explanation = ' | '.join(local_explanations)
+    global_explanation = ' | '.join(most_common_explanations)
     global_explanation_simplified = simplify_logic(global_explanation, 'dnf', force=True)
 
     # predictions based on FOL formula
@@ -98,7 +112,7 @@ def combine_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch
         minterm = minterm.replace('(', '').replace(')', '').split(' & ')
         local_predictions = np.ones(x.shape[0], dtype=bool)
         for terms in minterm:
-            terms = terms.split('f')
+            terms = terms.split('feature')
             if terms[0] == '~':
                 local_predictions *= ~x_bool[:, int(terms[1])]
             else:
@@ -106,13 +120,15 @@ def combine_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch
 
         predictions += local_predictions
 
-    feature_abbreviations = [f'f{i}' for i in range(len(concept_names))]
-    mapping = []
-    for f_abbr, f_name in zip(feature_abbreviations, concept_names):
-        mapping.append((f_abbr, f_name))
-
     global_explanation_simplified_str = str(global_explanation_simplified)
-    for k, v in mapping:
-        global_explanation_simplified_str = global_explanation_simplified_str.replace(k, v)
 
-    return global_explanation_simplified_str, predictions
+    if concept_names:
+        feature_abbreviations = [f'feature{i:05}' for i in range(len(concept_names))]
+        mapping = []
+        for f_abbr, f_name in zip(feature_abbreviations, concept_names):
+            mapping.append((f_abbr, f_name))
+
+        for k, v in mapping:
+            global_explanation_simplified_str = global_explanation_simplified_str.replace(k, v)
+
+    return global_explanation_simplified_str, predictions, counter_translated
