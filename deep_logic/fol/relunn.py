2f720481963247b11d8f016f07d4979d069a0dc9
Pietro <pietro.barbiero9@live.it>
2021-01-19 15:31:52 +0000

    First attempt for semi-local explanations

--- deep_logic/fol/relunn.py
+++ deep_logic/fol/relunn.py
@@ -1,29 +1,33 @@
 from typing import Tuple, List
 import collections
+import copy
 
 import torch
 import numpy as np
 from sympy import simplify_logic
 
+from .sigmoidnn import _build_truth_table
 from ..utils.base import collect_parameters
 from ..utils.relunn import get_reduced_model
 
 
-def generate_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch.Tensor,
-                                sample_id: int, concept_names: List = None,
-                                device: torch.device = torch.device('cpu')) -> str:
+def explain_semi_local(model: torch.nn.Module, x: torch.Tensor, y: torch.Tensor,
+                       x_sample: torch.Tensor, concept_names: List = None,
+                       device: torch.device = torch.device('cpu')) -> str:
     """
     Generate the FOL formula for a specific input.
 
     :param model: pytorch model
+    :param x: input data (train)
+    :param y: input labels (train)
     :param x_sample: input sample
-    :param k: upper bound to the number of symbols involved in the explanation (it controls the complexity of the explanation)
     :param concept_names: list containing the names of the input concepts
     :param device: cpu or cuda device
     :return: local explanation
     """
-    weights, _ = collect_parameters(model, device)
-    weights = weights[0][0]
+    reduced_model = get_reduced_model(model, x_sample)
+    w, bias = collect_parameters(reduced_model, device)
+    weights = copy.deepcopy(w[0][0])
 
     # if all the weights are zero, then the explanation is always 'False'
     w_abs = np.abs(weights)
@@ -34,33 +38,38 @@ def generate_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torc
     # sort features by weight importance
     sorted_features = np.argsort(-w_abs)
 
-    class_id = y[sample_id]
-    class_mask = (y!=class_id).squeeze()
-    x_opposite = x[class_mask]
-    y_opposite = y[class_mask]
-    x_sample = x[sample_id].unsqueeze(0)
-    y_sample = y[sample_id].unsqueeze(0)
-
-    x_data_sample = torch.cat([x_opposite, x_sample])
-    y_data_sample = torch.cat([y_opposite, y_sample])
-
-    # build explanation
+    # Build formula incrementally
+    x_sample = x_sample.unsqueeze(0)
+    y_pred_sample = (model(x_sample) > 0.5).to(torch.float)
+    mask = (y != y_pred_sample).squeeze()
+    x_validation = torch.cat([x[mask], x_sample]).to(torch.bool)
+    y_validation = torch.cat([y[mask], y_pred_sample]).squeeze()
+    y_preds_0 = torch.ones(y_validation.size(0), dtype=torch.bool)
+    # x_sample = x_sample > 0.5
     explanation = ''
-    for fj in sorted_features:
-        xij = x_sample[:, fj]
+    for j in sorted_features:
+        if x_sample[:, j] > 0.5:
+            y_preds = y_preds_0 * x_validation[:, j]
+        else:
+            y_preds = y_preds_0 * ~x_validation[:, j]
+
+        if y_preds.eq(y_validation).all().item():
+            break
 
         if explanation:
             explanation += ' & '
+        explanation += f'feature{j:010}' if x_sample[:, j] > 0.5 else f'~feature{j:010}'
 
-        term = f'feature{fj:05}'
-        if xij >= 0.5:
-            explanation += term
-        else:
-            explanation += f'~{term}'
+        y_preds_0 = y_preds
 
-        accuracy, _ = test_explanation(explanation, x_data_sample, y_data_sample)
-        if accuracy == 1:
-            break
+    # Simplify formula
+    for term in explanation.split(' & '):
+        explanation_simplified = copy.deepcopy(explanation)
+        explanation_simplified = explanation_simplified.replace(f'{term} & ', '')
+        if explanation_simplified:
+            accuracy, _ = test_explanation(explanation_simplified, x_validation, y_validation)
+            if accuracy == 1:
+                explanation = copy.deepcopy(explanation_simplified)
 
     if concept_names:
         explanation = replace_names(explanation, concept_names)
@@ -69,6 +78,14 @@ def generate_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torc
 
 
 def test_explanation(explanation: str, x: torch.Tensor, y: torch.Tensor) -> Tuple[float, np.ndarray]:
+    """
+    Test explanation
+
+    :param explanation: formula
+    :param x: input data
+    :param y: input labels
+    :return: Accuracy of the explanation and predictions
+    """
     minterms = str(explanation).split(' | ')
     x_bool = x.detach().numpy() > 0.5
     predictions = np.zeros(x.shape[0], dtype=bool)
@@ -89,7 +106,14 @@ def test_explanation(explanation: str, x: torch.Tensor, y: torch.Tensor) -> Tupl
 
 
 def replace_names(explanation: str, concept_names: List[str]) -> str:
-    feature_abbreviations = [f'feature{i:05}' for i in range(len(concept_names))]
+    """
+    Replace names of concepts in a formula.
+
+    :param explanation: formula
+    :param concept_names: new concept names
+    :return: Formula with renamed concepts
+    """
+    feature_abbreviations = [f'feature{i:010}' for i in range(len(concept_names))]
     mapping = []
     for f_abbr, f_name in zip(feature_abbreviations, concept_names):
         mapping.append((f_abbr, f_name))
@@ -118,17 +142,16 @@ def combine_local_explanations(model: torch.nn.Module, x: torch.Tensor, y: torch
     local_explanations_translated = []
     # TODO: multi class
     for sample_id, (xi, yi) in enumerate(zip(x, y)):
-        # get reduced model for each sample
-        model_reduced = get_reduced_model(model, xi)
-        output = model_reduced(xi)
+        # get prediction for each sample
+        output = model(xi)
 
         # generate local explanation only if the prediction is correct
         if output > 0.5 and (output > 0.5) == yi:
-            local_explanation = generate_local_explanations(model_reduced, x, y, sample_id,
-                                                            concept_names=None, device=device)
+            local_explanation = explain_semi_local(model, x, y, xi,
+                                                   concept_names=None, device=device)
             local_explanations.append(local_explanation)
-            local_explanation_translated = generate_local_explanations(model_reduced, x, y, sample_id,
-                                                                       concept_names=concept_names, device=device)
+            local_explanation_translated = explain_semi_local(model, x, y, xi,
+                                                              concept_names=concept_names, device=device)
             local_explanations_translated.append(local_explanation_translated)
 
     counter = collections.Counter(local_explanations)
