77b3d1715e96bb4f0738fd574f4733f0371d6d00
Pietro <pietro.barbiero9@live.it>
2021-01-21 12:16:00 +0000

    Update tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,27 +4,97 @@ import torch
 
 
 class TestTemplateObject(unittest.TestCase):
+    def test_relunets_pruning(self):
+        import numpy as np
+        from deep_logic.utils.relunn import get_reduced_model
+        from deep_logic.utils.base import validate_network, validate_data
+        from deep_logic.fol import explain_local, combine_local_explanations
+        import deep_logic as dl
+
+        torch.manual_seed(10)
+        np.random.seed(0)
+
+        x = torch.tensor([
+            [0, 0, 0.5, 0.5],
+            [0, 1, 0.5, 0.5],
+            [1, 0, 0.5, 0.5],
+            [1, 1, 0.5, 0.5],
+            [0.5, 0.5, 0, 0],
+            [0.5, 0.5, 0, 1],
+            [0.5, 0.5, 1, 0],
+            [0.5, 0.5, 1, 1],
+        ], dtype=torch.float)
+        y = torch.tensor([0, 1, 1, 0, 2, 3, 3, 3], dtype=torch.long)
+        n_classes = len(torch.unique(y))
+
+        layers = [
+            torch.nn.Linear(4, 20 * n_classes),
+            torch.nn.ReLU(),
+            dl.nn.XLinear(20, 5, n_classes),
+            torch.nn.ReLU(),
+            dl.nn.XLinear(5, 1, n_classes),
+            torch.nn.Softmax(),
+        ]
+        model = torch.nn.Sequential(*layers)
+        validate_network(model, 'relu')
+        validate_data(x)
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        loss_form = torch.nn.CrossEntropyLoss()
+        model.train()
+        need_pruning = True
+        for epoch in range(1000):
+            optimizer.zero_grad()
+            y_pred = model(x)
+            loss = loss_form(y_pred, y)
+            for module in model.children():
+                if isinstance(module, torch.nn.Linear):
+                    loss += 0.0005 * torch.norm(module.weight, 1)
+            loss.backward()
+            optimizer.step()
+
+            if epoch > 500 and need_pruning:
+                dl.utils.relunn.prune_features(model, n_classes)
+                need_pruning = False
+
+            # compute accuracy
+            if epoch % 100 == 0:
+                y_pred_d = torch.argmax(y_pred, dim=1)
+                accuracy = y_pred_d.eq(y).sum().item() / y.size(0)
+                print(f'Epoch {epoch}: train accuracy: {accuracy:.4f}')
+
+        explanation = explain_local(model, x, y, x[1], concept_names=['f1', 'f2', 'f3', 'f4'])
+        print(explanation)
+
+        for target_class in range(n_classes):
+            global_explanation, _, counter = combine_local_explanations(model, x, y, target_class=target_class,
+                                                                        topk_explanations=5,
+                                                                        concept_names=['f1', 'f2', 'f3', 'f4'])
+            print(f'Target class: {target_class} - Explanation: {global_explanation}')
+            print(f'\t{counter}')
+
+        return
+
     def test_relunets(self):
         import numpy as np
         from deep_logic.utils.relunn import get_reduced_model
         from deep_logic.utils.base import validate_network, validate_data
-        from deep_logic.fol import explain_semi_local, combine_local_explanations
+        from deep_logic.fol import explain_local, combine_local_explanations
+        import deep_logic as dl
 
         torch.manual_seed(10)
         np.random.seed(0)
 
         x = torch.tensor([[0, 1], [1, 1], [1, 0], [0, 0]], dtype=torch.float)
-        y = torch.tensor([1, 0, 1, 0], dtype=torch.float).unsqueeze(1)
-        x_test = torch.tensor([[0, 1], [1, 1], [0, 0], [1, 0], [1, 0], [1, 0]], dtype=torch.float)
-        y_test = torch.tensor([1, 0, 0, 1, 1, 1], dtype=torch.float).unsqueeze(1)
-        x_sample = torch.tensor([0, 1], dtype=torch.float)
+        y = torch.tensor([1, 0, 1, 0], dtype=torch.long)
+        n_classes = len(torch.unique(y))
 
         layers = [
-            torch.nn.Linear(2, 10),
+            torch.nn.Linear(2, 10 * n_classes),
             torch.nn.ReLU(),
-            torch.nn.Linear(10, 4),
+            dl.nn.XLinear(10, 4, n_classes),
             torch.nn.ReLU(),
-            torch.nn.Linear(4, 1),
+            dl.nn.XLinear(4, 1, n_classes),
             torch.nn.Sigmoid(),
         ]
         model = torch.nn.Sequential(*layers)
@@ -32,23 +102,38 @@ class TestTemplateObject(unittest.TestCase):
         validate_data(x)
 
         optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        loss_form = torch.nn.CrossEntropyLoss()
         model.train()
+        need_pruning = True
         for epoch in range(1000):
             optimizer.zero_grad()
             y_pred = model(x)
-            loss = torch.nn.functional.binary_cross_entropy(y_pred, y)
+            loss = loss_form(y_pred, y)
             for module in model.children():
                 if isinstance(module, torch.nn.Linear):
                     loss += 0.0001 * torch.norm(module.weight, 1)
             loss.backward()
             optimizer.step()
 
-        model_reduced = get_reduced_model(model, x_sample)
-        explanation = explain_semi_local(model, x, y, x_sample, concept_names=['f0', 'f1'])
+            if epoch > 500 and need_pruning:
+                dl.utils.relunn.prune_features(model, n_classes)
+                need_pruning = False
+
+            # compute accuracy
+            if epoch % 100 == 0:
+                y_pred_d = torch.argmax(y_pred, dim=1)
+                accuracy = y_pred_d.eq(y).sum().item() / y.size(0)
+                print(f'Epoch {epoch}: train accuracy: {accuracy:.4f}')
+
+        explanation = explain_local(model, x, y, x[1], concept_names=['f1', 'f2'])
         print(explanation)
 
-        explanation = combine_local_explanations(model, x_test, y_test)
-        print(explanation)
+        for target_class in range(n_classes):
+            global_explanation, _, counter = combine_local_explanations(model, x, y, target_class=target_class,
+                                                                        topk_explanations=5,
+                                                                        concept_names=['f1', 'f2', 'f3', 'f4'])
+            print(f'Target class: {target_class} - Explanation: {global_explanation}')
+            print(f'\t{counter}')
 
         return
 
