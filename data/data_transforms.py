1f14e6a6945580226910156d3445fd4d3455e60f
Gaber <gabriele.ciravegna@unifi.it>
2021-01-25 13:04:31 +0100

    Modified transformers:
    - avoided random resize crop in training transform
    - inverted centercrop and resize for validation (in order to get most of the image)
    - added other type of data augmentation

--- data/data_transforms.py
+++ data/data_transforms.py
@@ -1,45 +1,29 @@
-import torch
-from PIL import ImageEnhance
 from torchvision import transforms
 from . import CUB200
 
-transform_type_dict = dict(Brightness=ImageEnhance.Brightness, Contrast=ImageEnhance.Contrast,
-                           Sharpness=ImageEnhance.Sharpness, Color=ImageEnhance.Color)
 
-
-class ImageJitter(object):
-	def __init__(self, transform_dict):
-		self.transforms = [(transform_type_dict[k], transform_dict[k]) for k in transform_dict]
-
-	def __call__(self, img):
-		out = img
-		randtensor = torch.rand(len(self.transforms))
-		for i, (transformer, alpha) in enumerate(self.transforms):
-			r = alpha * (randtensor[i] * 2.0 - 1.0) + 1
-			out = transformer(out).enhance(r).convert('RGB')
-		return out
-
-
-def get_transform(dataset, data_augmentation=False, inception=False):
+def get_transform(dataset, data_augmentation=False, inception=False) -> transforms.Compose:
 	size = 299 if inception else 224
 	if dataset == CUB200:
 		if data_augmentation:
 			transform = transforms.Compose([
-					transforms.RandomResizedCrop(size=size),
-					ImageJitter({"Brightness": 0.4, "Contrast": 0.4, "Color": 0.4}),
-					transforms.RandomHorizontalFlip(),
-					transforms.ToTensor(),
-					transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-				])
+				# transforms.RandomResizedCrop(size=size),
+				transforms.Resize(size=size),
+				transforms.CenterCrop(size=size),
+				transforms.ColorJitter(brightness=0.4, contrast=0.4, hue=0.4, saturation=0.4),
+				transforms.RandomRotation(0.4),
+				transforms.RandomHorizontalFlip(),
+				transforms.RandomVerticalFlip(),
+				transforms.ToTensor(),
+				transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+			])
 		else:
 			transform = transforms.Compose([
-				transforms.CenterCrop(size=size),
 				transforms.Resize(size=size),
+				transforms.CenterCrop(size=size),
 				transforms.ToTensor(),
 				transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 			])
 	else:
 		raise NotImplementedError()
 	return transform
-
-
