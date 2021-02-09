72910c24991c4a738609a3aee92ef52e8dce644c
Gaber <gabriele.ciravegna@unifi.it>
2021-02-09 11:39:02 +0100

    Corrected merge

--- deep_logic/__init__.py
+++ deep_logic/__init__.py
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
-from . import fol
 from . import utils
 from . import models
-=======
 from . import logic
 from . import utils
->>>>>>> master
 from . import nn
 
 from ._version import __version__
 
 __all__ = [
     'nn',
-<<<<<<< HEAD
     'models',
-=======
->>>>>>> master
-    'fol',
+    'logic',
     'utils',
     '__version__'
 ]
