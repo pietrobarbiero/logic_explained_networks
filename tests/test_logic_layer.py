190c37dbf6f86e1b9244b149147e7bc00fe0c34f
Pietro Barbiero <barbiero@tutanota.com>
2021-03-17 09:50:01 +0100

    Update unittests for logic layer

--- tests/test_logic_layer.py
+++ tests/test_logic_layer.py
@@ -5,6 +5,7 @@ import torch
 import deep_logic as dl
 from deep_logic.logic import explain_class
 from deep_logic.utils.base import set_seed
+from deep_logic.utils.layer import prune_logic_layers
 
 
 class TestTemplateObject(unittest.TestCase):
@@ -22,13 +23,68 @@ class TestTemplateObject(unittest.TestCase):
             y = torch.tensor([0, 1, 1, 0], dtype=torch.float)
 
             layers = [
-                dl.nn.XLogic(2, activation='identity', first=True),
-                torch.nn.Linear(2, 5),
+                dl.nn.XLogic(2, 5, first=True),
                 torch.nn.LeakyReLU(),
-                dl.nn.XLogic(5, activation='sigmoid'),
+                torch.nn.Linear(5, 5),
+                torch.nn.LeakyReLU(),
                 torch.nn.Linear(5, 1),
+                dl.nn.XLogic(1, 1, top=True),
+            ]
+            model = torch.nn.Sequential(*layers)
+
+            optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+            loss_form = torch.nn.BCELoss()
+            model.train()
+            for epoch in range(2000):
+                optimizer.zero_grad()
+                y_pred = model(x).squeeze()
+                loss = loss_form(y_pred, y)
+
+                loss.backward()
+                optimizer.step()
+
+                # compute accuracy
+                if epoch % 100 == 0:
+                    y_pred_d = y_pred > 0.5
+                    accuracy = y_pred_d.eq(y).sum().item() / y.size(0)
+                    print(f'Epoch {epoch}: loss {loss:.4f} train accuracy: {accuracy:.4f}')
+
+            class_explanation, class_explanations = explain_class(model, x, y, binary=True,
+                                                                  target_class=0,
+                                                                  topk_explanations=10)
+            print(class_explanation)
+            print(class_explanations)
+            assert class_explanation == '(feature0000000000 & feature0000000001) | (~feature0000000000 & ~feature0000000001)'
+
+            class_explanation, class_explanations = explain_class(model, x, y, binary=True,
+                                                                  target_class=1,
+                                                                  topk_explanations=10)
+            print(class_explanation)
+            print(class_explanations)
+            assert class_explanation == '(feature0000000000 & ~feature0000000001) | (feature0000000001 & ~feature0000000000)'
+
+        return
+
+    def test_explain_class_binary_pruning(self):
+        for i in range(20):
+            set_seed(i)
+
+            # Problem 1
+            x = torch.tensor([
+                [0, 0, 0, 1],
+                [0, 1, 0, 1],
+                [1, 0, 0, 1],
+                [1, 1, 0, 1],
+            ], dtype=torch.float)
+            y = torch.tensor([0, 1, 1, 0], dtype=torch.float)
+
+            layers = [
+                dl.nn.XLogic(4, 5, first=True),
                 torch.nn.LeakyReLU(),
-                dl.nn.XLogic(1, activation='sigmoid', top=True),
+                torch.nn.Linear(5, 5),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(5, 1),
+                dl.nn.XLogic(1, 1, top=True),
             ]
             model = torch.nn.Sequential(*layers)
 
@@ -40,31 +96,19 @@ class TestTemplateObject(unittest.TestCase):
                 optimizer.zero_grad()
                 y_pred = model(x).squeeze()
                 loss = loss_form(y_pred, y)
-                # loss = 0
 
                 for module in model.children():
                     if isinstance(module, dl.nn.XLogic):
-                        # loss += 0.0001 * torch.norm(module.weight, 1)
-                        # loss += 0.0001 * torch.norm(module.bias, 1)
-                        # break
-                        if not module.first and not module.top:
-                            cov = 1 / (module.conceptizator.concepts.shape[1] - 1) * \
-                                  torch.matmul(module.conceptizator.concepts.T, module.conceptizator.concepts)
-                            cov_np = cov.cpu().detach().numpy()
-                            cov_obj = torch.eye(module.conceptizator.concepts.shape[1])
-                            loss += 0.001 * torch.norm(cov - cov_obj, p=2)
-                            # loss += 1-(module.symbols > 0.5).sum(dim=1).to(float).mean()
-                            # cov = 1 / (module.symbols.shape[1] - 1) * torch.matmul(module.symbols.T, module.symbols)
-                            # cov_obj = torch.eye(module.symbols.shape[1])
-                            # loss += torch.norm(torch.abs(cov - cov_obj), p=1)
-                            # break
+                        loss += 0.0001 * torch.norm(module.weight, 1)
+                        loss += 0.0001 * torch.norm(module.bias, 1)
+                        break
 
                 loss.backward()
                 optimizer.step()
 
-                # if epoch > 1000 and need_pruning:
-                #     dl.utils.layer.prune_logic_layers(model)
-                #     need_pruning = False
+                if epoch > 1000 and need_pruning:
+                    prune_logic_layers(model, fan_in=2)
+                    need_pruning = False
 
                 # compute accuracy
                 if epoch % 100 == 0:
@@ -102,49 +146,25 @@ class TestTemplateObject(unittest.TestCase):
             y = torch.tensor([0, 1, 1, 2], dtype=torch.long)
 
             layers = [
-                dl.nn.XLogic(2, activation='identity', first=True),
-                torch.nn.Linear(2, 5),
+                dl.nn.XLogic(2, 5, first=True),
                 torch.nn.LeakyReLU(),
-                # dl.nn.XLogic(5, activation='sigmoid'),
+                torch.nn.Linear(5, 5),
+                torch.nn.LeakyReLU(),
                 torch.nn.Linear(5, 3),
-                torch.nn.LeakyReLU(),
-                dl.nn.XLogic(3, activation='identity', top=True),
+                dl.nn.XLogic(3, 3, activation='identity', top=True),
             ]
             model = torch.nn.Sequential(*layers)
 
-            optimizer = torch.optim.AdamW(model.parameters(), lr=0.03)
+            optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
             loss_form = torch.nn.CrossEntropyLoss()
             model.train()
-            need_pruning = True
             for epoch in range(2000):
                 optimizer.zero_grad()
                 y_pred = model(x)
                 loss = loss_form(y_pred, y)
-                # loss = 0
-
-                for module in model.children():
-                    if isinstance(module, dl.nn.XLogic):
-                        loss += 0.0001 * torch.norm(module.weight, 1)
-                        loss += 0.0001 * torch.norm(module.bias, 1)
-                        # break
-                        if not module.first and not module.top:
-                            cov = 1 / (module.conceptizator.concepts.shape[1] - 1) * \
-                                  torch.matmul(module.conceptizator.concepts.T, module.conceptizator.concepts)
-                            cov_np = cov.cpu().detach().numpy()
-                            cov_obj = torch.eye(module.conceptizator.concepts.shape[1])
-                            loss += 0.001 * torch.norm(cov - cov_obj, p=2)
-                            # loss += 1-(module.symbols > 0.5).sum(dim=1).to(float).mean()
-                            # cov = 1 / (module.symbols.shape[1] - 1) * torch.matmul(module.symbols.T, module.symbols)
-                            # cov_obj = torch.eye(module.symbols.shape[1])
-                            # loss += torch.norm(torch.abs(cov - cov_obj), p=1)
-                            # break
 
                 loss.backward()
                 optimizer.step()
-
-                # if epoch > 1000 and need_pruning:
-                #     dl.utils.layer.prune_logic_layers(model)
-                #     need_pruning = False
 
                 # compute accuracy
                 if epoch % 100 == 0:
