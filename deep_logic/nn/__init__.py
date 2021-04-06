43aac653579a20c6864c48cec525ab9b793590fc
Pietro Barbiero <barbiero@tutanota.com>
2021-04-06 17:59:25 +0200

    Add DisentangledConcepts layer

--- deep_logic/nn/__init__.py
+++ deep_logic/nn/__init__.py
@@ -1,9 +1,10 @@
 from .linear import XLinear
-from .logic import XLogic
+from .logic import XLogic, DisentangledConcepts
 from .concepts import Conv2Concepts
 
 __all__ = [
     'XLogic',
+    'DisentangledConcepts',
     'XLinear',
     'Conv2Concepts',
 ]
